c9fe8a80416da4d7fb3ff90e955166f25669b751
carolmota18 <carolinemota78@gmail.com>
2023-12-12 14:23:17 -0300

    desafio feito

---
+++ script.js
@@ -0,0 +1,34 @@
+console.log("Ano novo está chegando");
+
+let  nome = "Mariah Carey";
+
+let  idade = 54;
+
+let ano = 2023;
+
+const anoNascimento = ano - idade;
+
+console.log(anoNascimento);
+
+let quartaFeira = true;
+
+console.log(quartaFeira);
+
+console.log(typeof quartaFeira);
+
+let $idade = 22;
+
+if($idade >= 18){
+    console.log("poderá curtir o show")
+}else{ 
+    console.log("simto muito , mas vai ter que assiter na Tv globo")
+}
+
+let niver = "Maio"
+
+if(niver == "Agusto" || niver ==  "Dezenbro" || niver == "Junho"){
+    console.log("Umas ou mais opç~~oes um mais opções esta corretas")
+}else{
+    console.log(`Ano de errado não esta certo , o mes digitado foi ${niver}`)
+}
+
